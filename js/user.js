7702f4c718bde31fa928623c38dc059a897ab348
Sergey Maximov <serj.max@gmail.com>
2014-04-16 18:10:34 +0300

    не пишется в поиске в индексе

---
+++ js/user.js
@@ -0,0 +1,11 @@
+$(function () {
+
+	$('#index-hero-search').toggle(
+		function () {
+			$('#menu-item').css('display','block');
+		}, function () {
+			$('#menu-item').css('display','none');
+	});
+	
+
+});
